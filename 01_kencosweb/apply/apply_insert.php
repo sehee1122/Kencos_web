51c6f06bfe40e1d4a1666a7fa3e398c470ae8413
sehee1122 <55127603+sehee1122@users.noreply.github.com>
2020-09-11 04:54:50 +0900

    Homepage connect
    Commits on Sep 11, 2020

--- 01_kencosweb/apply/apply_insert.php
+++ 01_kencosweb/apply/apply_insert.php
@@ -14,17 +14,17 @@
     <body>
         <?php
             //mysql 접속 계정 정보 설정
-            $host = "localhost"; 
-            $user = "root";
-            $password = "1122";
-            $database = "medical_db";
+            $host = "keenus13.cafe24.com"; // 183.111.174.52
+            $user = "username";
+            $password = "password";
+            $database = "keenus13";
 
-            $ka_name = $_POST['name'];
-            $ka_phone1 = $_POST['phone1'];
-            $ka_phone2 = $_POST['phone2'];
-            $ka_phone3 = $_POST['phone3'];
-            $ka_email = $_POST['email'];
-            $ka_agree = $_POST['agree'];
+            $ka_name = $_POST['your-name'];
+            $ka_phone1 = $_POST['tel-830'];
+            $ka_phone2 = $_POST['tel-831'];
+            $ka_phone3 = $_POST['tel-832'];
+            $ka_email = $_POST['your-email'];
+            $ka_agree = $_POST['your-consent'];
 
             $ka_phone = $ka_phone1."-".$ka_phone2."-".$ka_phone3; // xxx-xxxx-xxxx
 
@@ -41,7 +41,7 @@
             ?>
 
             <p>
-                <a href="apply_add.php">Try again</a>
+                <a href="index.html">Try again</a>
             </p>
     </body>
 </html>
