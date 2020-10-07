7f960a45cc403dffcae5b7e554fbabdb5e0e5bc8
sehee1122 <55127603+sehee1122@users.noreply.github.com>
2020-10-07 14:07:26 +0900

    Delete index.js

--- 03_PacsPlus/ppnw/web-viewer/src/containers/index.js
+++
@@ -1,6 +0,0 @@
-const req = require.context('.', false, /^((?!index).)*\.vue$/)
-
-req.keys().forEach((key) => {
-  const containerName = key.replace(/^\.\/([^.]+)\.vue$/, '$1')
-  module.exports[containerName] = req(key).default
-})
