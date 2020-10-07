dced39d6793976de1bc1c926e2bf5552a330ac6e
sehee1122 <55127603+sehee1122@users.noreply.github.com>
2020-10-07 14:11:31 +0900

    Delete index.js

--- 03_PacsPlus/ppnw/api-server/src/api/v1/worklist/index.js
+++
@@ -1,8 +0,0 @@
-const Router = require('koa-router');
-
-const worklist = new Router();
-const worklistController = require('./worklist.controller');
-
-worklist.get('/', worklistController.worklist);
-
-module.exports = worklist;
